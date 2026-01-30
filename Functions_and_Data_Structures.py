b931cfacc0d53d7db2070616e3fffc909758d6d1
jhanerose <jhanerosesadicon@gmail.com>
2026-01-30 14:53:52 +0800

    Add Functions_and_Data_Structures.py

---
+++ Functions_and_Data_Structures.py
@@ -0,0 +1,9 @@
+# Functions and Data Structures
+def calculate_area(length, width):
+    return length * width
+
+length = float(input("Enter length: "))
+width = float(input("Enter width: "))
+
+area = calculate_area(length, width)
+print("The area of the rectangle is:", area)
